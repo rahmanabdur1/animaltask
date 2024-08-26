9d1b520ee91e1f1142bc11f4a3f874b66d529a73
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-08-26 22:01:01 +0300

    first commit

--- src/app/addcategory/page.js
+++ src/app/addcategory/page.js
@@ -8,9 +8,9 @@ const AddCategory = () => {
            <div className="bg-white p-8 rounded-lg shadow-lg text-black w-[300px] relative">
                {/* Close Button */}
                <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" >
-               <Link href={'/'}>Back</Link>
+               <Link href={'/'}>Back Home</Link>
                </button>
-               <h2 className="text-xl font-semibold mb-4">Add Category</h2>
+               <h2 className="text-xl font-semibold mb-4">Add  Category</h2>
                <AddCategorysForm/>
            </div>
        </div>
