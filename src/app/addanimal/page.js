6d00951d58a94e4095331b0bf491898563e8bedc
rahmanabdur1 <rahmanabdurr65@gmail.com>
2024-08-26 21:30:02 +0300

    code added

--- src/app/addanimal/page.js
+++ src/app/addanimal/page.js
@@ -11,7 +11,7 @@ const AddAnimal = () => {
                         <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" >
                         <Link href={'/'}>Back</Link>
                         </button>
-                        <h2 className="text-xl font-semibold mb-4">Add Category</h2>
+                        <h2 className="text-xl font-semibold mb-4">Add Animal</h2>
                         <AddAnimalForm />
                     </div>
                 </div>
--- src/app/addcategory/page.js
+++ src/app/addcategory/page.js
@@ -1,23 +1,21 @@
-
 import AddCategorysForm from '@/Components/AddCategorysForm';
-import Link from 'next/link';
 import React from 'react';
-
-const AddCategoryPage = () => {
+import Link from 'next/link';
+const AddCategory = () => {
     return (
         <div>
-               <div className="fixed inset-0 bg-[rgba(0,0,0,0.5)] z-[100] flex justify-center items-center">
-                    <div className="bg-white p-8 rounded-lg shadow-lg text-black w-[300px] relative">
-                        {/* Close Button */}
-                        <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" >
-                        <Link href={'/'}>Back</Link>
-                        </button>
-                        <h2 className="text-xl font-semibold mb-4">Add Animal</h2>
-                      <AddCategorysForm/>
-                    </div>
-                </div>
-        </div>
+        <div className="fixed inset-0 bg-[rgba(0,0,0,0.5)] z-[100] flex justify-center items-center">
+           <div className="bg-white p-8 rounded-lg shadow-lg text-black w-[300px] relative">
+               {/* Close Button */}
+               <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" >
+               <Link href={'/'}>Back</Link>
+               </button>
+               <h2 className="text-xl font-semibold mb-4">Add Category</h2>
+               <AddCategorysForm/>
+           </div>
+       </div>
+</div>
     );
 };
 
-export default AddCategoryPage;
+export default AddCategory;
